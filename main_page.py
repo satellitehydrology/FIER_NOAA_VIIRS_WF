4e1c1c079116b34d801fc716096ab9a1dd0b4eb6
Chi-Hung Chang <jack7989@gmail.com>
2024-02-05 08:47:09 -0600

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -14,6 +14,7 @@ import branca
 import branca.colormap as cm
 from scipy import interpolate
 from xyzservices.lib import TileProvider
+import ssl
 
 
 ssl._create_default_https_context = ssl._create_stdlib_context
