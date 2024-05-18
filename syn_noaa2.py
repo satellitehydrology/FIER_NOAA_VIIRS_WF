a5dc7a024011ec44e2e72b346d423b133dd260ed
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-05-18 13:33:06 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -262,7 +262,7 @@ def run_fier(AOI_str, doi, in_run_type):
         except Exception as e:
             print(f"Error loading model: {e}")
             
-        in_model = models.load_model(TF_model_path+'site-'+str(site)+'_tpc'+str(mode).zfill(2))
+        #in_model = models.load_model(TF_model_path+'site-'+str(site)+'_tpc'+str(mode).zfill(2))
 
         in_good_hydro = doi_fct_q
         tf_good_hydro = tf.data.Dataset.from_tensors(in_good_hydro)
