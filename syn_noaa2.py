5682ede833f787a1213778c2ee9c9bd2b3b689cd
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-05-20 13:35:52 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -1,6 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow.keras import keras
+import tensorflow.keras as keras
 #import tensorflow.compat.v1 as tf
 
 #tf.compat.v1.disable_v2_behavior()
