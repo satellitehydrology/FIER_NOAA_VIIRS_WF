fb50a0e8eaf1471d9536c1b6dda1b91a600c1963
Hung-Hsien Wan <24313928+Hung-HsienWan@users.noreply.github.com>
2024-05-20 11:37:27 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -260,12 +260,13 @@ def run_fier(AOI_str, doi, in_run_type):
 
         #model_directory = r'AOI/MississippiRiver/TF_model/site-07024175_tpc01'
         model_directory = TF_model_path+'site-'+str(site)+'_tpc'+str(mode).zfill(2)
+        '''
         #try:
         with tf.Session(graph=tf.Graph()) as sess:
             st.write('open sucess')
             tf.saved_model.loader.load(sess, [tf.saved_model.SERVING], model_directory)
             print("Model loaded successfully in TensorFlow 1.x compatibility mode.")
-        '''
+        
         except Exception as e:
             st.write(model_directory)
         
