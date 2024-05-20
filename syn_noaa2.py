c4f589e05bad2ee7d93c8e1f4c71e5f438d9ee66
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-05-20 16:03:28 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -193,7 +193,7 @@ def perf_qm_mon(fct_syn_wf_stack, fct_date, qm_scaling_path, qm_mask):
 
     return fct_syn_wf_stack
 
-def run_fier(AOI_str, doi, in_run_type):
+def run_fier(AOI_str, doi, in_run_type, in_run_type2):
     """
     This function read the AOI, DOI, forecasting run type to synthesized forecasted water fraction
 
@@ -261,7 +261,7 @@ def run_fier(AOI_str, doi, in_run_type):
         
             doi_fct_datetime = fct_datetime[doi_indx]
             doi_fct_q = (pd.DataFrame(JSON_object[0]["data"])['value'][doi_indx]*0.0283168).mean()
-            if in_run_type=='medium_range_ensemble_mean_bias_corrected': 
+            if in_run_type2=='medium_range_ensemble_mean_bias_corrected': 
                 with open(model_path + 'interpolated_function'+str(nwm_site) + '.pkl', 'rb') as file:
                     bc_model = pickle.load(file)
                 doi_fct_q = bc_model (doi_fct_q) 
