1a6a97cc5d034f0569ae249a8416be7d345e67cb
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-05-20 15:41:27 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -261,9 +261,10 @@ def run_fier(AOI_str, doi, in_run_type):
         
             doi_fct_datetime = fct_datetime[doi_indx]
             doi_fct_q = (pd.DataFrame(JSON_object[0]["data"])['value'][doi_indx]*0.0283168).mean()
-            with open(model_path + 'interpolated_function'+str(nwm_site) + '.pkl', 'rb') as file:
-                bc_model = pickle.load(file)
-            doi_fct_q = bc_model (doi_fct_q) 
+            if in_run_type=='medium_range_ensemble_mean_bias_corrected' 
+                with open(model_path + 'interpolated_function'+str(nwm_site) + '.pkl', 'rb') as file:
+                    bc_model = pickle.load(file)
+                doi_fct_q = bc_model (doi_fct_q) 
 
         #model_directory = r'AOI/MississippiRiver/TF_model/site-07024175_tpc01'
         model_directory = TF_model_path+'site-'+str(site)+'_tpc'+str(mode).zfill(2)
