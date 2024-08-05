3e2dc197983b62cbee8bfcebc2a710c9c57810f0
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-08-05 16:05:25 -0500

    Update main_page2.py

--- main_page2.py
+++ main_page2.py
@@ -226,7 +226,7 @@ with row1_col2:
             if AOI_str=='MississippiRiver':
                 exp_fct_indata = {'time':xr.load_dataarray('AOI/'+AOI_str+'/nwm_archive/medium_lt08_App_biascorrected.nc').time.data}
             elif AOI_str=='RedRiver':
-                exp_fct_indata = {'time':xr.load_dataarray('AOI/'+AOI_str+'/nwm_archive/medium_lt08_App.nc').time.data}
+                exp_fct_indata = {'time':xr.load_dataarray('AOI/'+AOI_str+'/nwm_archive/medium_lt08_App.nc' , engine='h5netcdf').time.data}
             exp_fct_data = pd.DataFrame(exp_fct_indata)['time']
             exp_fct_time = pd.to_datetime(exp_fct_data)
             
