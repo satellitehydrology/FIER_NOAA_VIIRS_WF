841171f66b938b9225c2f95cb68f7addfafe93f8
Amirhossein Rostami <153062776+rostamiamirhs@users.noreply.github.com>
2024-08-05 16:39:42 -0500

    Update syn_noaa2.py

--- syn_noaa2.py
+++ syn_noaa2.py
@@ -222,7 +222,7 @@ def run_fier(AOI_str, doi, in_run_type, in_run_type2):
     if AOI_str=='MississippiRiver':
         nwm_archive_path = 'medium_lt08_tot.nc'
         nwm_bias_corrected_archive_path = 'AOI/'+AOI_str+'/nwm_archive/medium_lt08_App_biascorrected.nc'
-        nwm_bias_corrected_archive = xr.load_dataarray(nwm_bias_corrected_archive_path)
+        nwm_bias_corrected_archive = xr.load_dataarray(nwm_bias_corrected_archive_path, engine = 'h5netcdf')
     elif AOI_str=='RedRiver': 
         nwm_archive = xr.load_dataarray(nwm_archive_path, engine = 'h5netcdf')
         nwm_bias_corrected_archive_path = 'AOI/'+AOI_str+'/nwm_archive/medium_lt08_App.nc'
